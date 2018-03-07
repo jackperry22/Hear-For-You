92065626f261b06fe3cc9377bfd211c457a4e997
Jack Perry <jackperry@192-168-1-113.tpgi.com.au>
2018-03-07 13:40:14 +1100

    Initial Commit

---
+++ Hear for youTests/Hear_for_youTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hear_for_youTests.swift
+//  Hear for youTests
+//
+//  Created by Jack Perry on 7/3/18.
+//  Copyright © 2018 Jack Perry. All rights reserved.
+//
+
+import XCTest
+@testable import Hear_for_you
+
+class Hear_for_youTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
